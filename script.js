ce0b5aed4915fb16c0a451a02daa71f5bdb34403
kaiqueazevedo <kaiqueacastilho@gmail.com>
2022-04-30 18:28:44 -0300

    x

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+//sessão musica
+
+//let musica = [
+  //  {titulo:'Termina Comigo Antes', artista:'Gusttavo Lima', src:'musica/gusttavolima1.mp3', img:'imagens/gusttavolima6.webp'},
+  //  {titulo:'Na Hora de Amar ', artista:'Gusttavo Lima', src:'musica/gusttavolima3.mp3', img:'imagens/gusttavolima6.webp'} , 
+  //  {titulo:'Te amo Cada Vez Mais', artista:'Gusttavo Lima',src:'musica/gusttavolima2.mp3', img:'imagens/gusttavolima6.webp'}
+//];
+
+//function renderizarMusica(index){
+  //  source.setAttribute('src',source[index].src);
+   // source.addEventListener('loadeddata',() =>{
+    //    nomeMusica.textContent = source[index]. tutilo;
+     //   nomeArtista.textContent = source[index].artista;
+      //  imagem.src = source[index].img;
+      //  duracaoSource.textContent = segundosParaMinutos(Math.floor(source.duration));
+
+
+document.getElementById("anterior").addEventListener('click',() =>{
+    indexSource--;
+    renderizarMusica(indexSource)
+});
+document.getElementById("proxima").addEventListener('click',() =>{
+    indexSource++;
+    renderizarMusica(indexSource);
+});
+
 // alterar imagem de playPause
 
 let image = document.getElementById("play");
@@ -14,9 +40,11 @@ imagem.setAttribute("src","./imagens/pausa_icon.png")
 
 })
 
+//duracaoSourcetextContent = segundosParaMinutos(Math.floor(source.duration));
+
 // funções de controle  da musica
 let source = document.getElementById("music")
-//let music = audio.controls
+let music = audio.controls
 
 function retroceder() {
     source.currentTime -= 15;
@@ -40,10 +68,3 @@ function pause() {
     document.getElementById("pause").style.display = "none";
     document.getElementById("play").style.display = "inline";
 }
-
-// sessao musica
-
-let musica =[
- i
-
-]
